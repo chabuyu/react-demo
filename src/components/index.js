4fcd089c62699d3ce7d1c1143ed90c097643a5ab
chabuyu <1474273534@qq.com>
2019-03-13 11:40:52 +0800

    feat:npm run build

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Common from './Common'
-
-export{
-  Common
-}
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import user from './user'
-import message from './message'
-
-export default combineReducers({
-  user,
-  message,
-
-})
